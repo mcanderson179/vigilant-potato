2e8d99a3e20b220ecb1035bb0343fe69af293389
Michael Anderson <90523886+mcanderson179@users.noreply.github.com>
2024-02-05 07:56:51 -0600

    Added notes to ssh_cmd.py
    Notes to explain what some obscure sections of the code do

--- ssh_cmd.py
+++ ssh_cmd.py
@@ -3,7 +3,9 @@ import getpass
 import sys
 
 def ssh_command(ip, user, passwd, port):
+#   This next line deals with socket management
     client = paramiko.SSHClient()
+#   The next line is for warning the client if the key received from the server is untrusted
     client.set_missing_host_key_policy(paramiko.WarningPolicy())
 
 
@@ -17,7 +19,7 @@ def ssh_command(ip, user, passwd, port):
         stdin, stdout, stderr = client.exec_command(cmd)
         stdout.channel.recv_exit_status()
         output = stdout.read().decode()
-        print('Returned output')
+        print('Returned output: ')
         print(output)
     except Exception as e:
         client.close()
